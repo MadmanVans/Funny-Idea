4584ff475499044bf7d4ae059b26c991a370ea46
Van <852114705@qq.com>
2019-10-24 00:02:06 +0800

    初步修改

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-// window.onload = function () {
-//     new IScroll(document.querySelector(".zh_scroll_bar"), {
-//         scrollX: true,
-//         scrollY: false
-//     });
-//     new IScroll(document.querySelector(".zh_sort_left"), {
-//         scrollX: false,
-//         scrollY: true
-//     });
-//     new IScroll(document.querySelector(".zh_sort_right"), {
-//         scrollX: false,
-//         scrollY: true
-//     });
-// }
+window.onload = function () {
+    new IScroll(document.querySelector(".zh_scroll_bar"), {
+        scrollX: true,
+        scrollY: false
+    });
+    new IScroll(document.querySelector(".zh_sort_left"), {
+        scrollX: false,
+        scrollY: true
+    });
+    new IScroll(document.querySelector(".zh_sort_right"), {
+        scrollX: false,
+        scrollY: true
+    });
+}
+$(function(){
+    $('#myTabs a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+    $('.carousel').carousel()
+});
