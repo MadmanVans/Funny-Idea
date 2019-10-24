2a876f08461391fb9ad68bd7debd57f1d28c78f9
Van <852114705@qq.com>
2019-10-24 16:33:09 +0800

    初步修改

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
+// 问题:无法实现滚动效果
 window.onload = function () {
-    new IScroll(document.querySelector(".zh_scroll_bar"), {
+    new IScroll(document.querySelector(".zh_scroll"), {
         scrollX: true,
         scrollY: false
     });
-    new IScroll(document.querySelector(".zh_sort_left"), {
+    new IScroll(document.querySelector(".zh_sortLeft"), {
         scrollX: false,
         scrollY: true
     });
-    new IScroll(document.querySelector(".zh_sort_right"), {
+    new IScroll(document.querySelector(".zh_sortRight"), {
         scrollX: false,
         scrollY: true
     });
 }
 $(function(){
-    $('#myTabs a').click(function (e) {
-        e.preventDefault()
-        $(this).tab('show')
-    })
     $('.carousel').carousel()
 });
