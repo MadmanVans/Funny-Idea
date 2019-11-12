52fc00eb77c31aaf8cfa8ec0290903f2e2327070
Van <852114705@qq.com>
2019-11-13 01:59:31 +0800

    初步修改

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-// 问题:无法实现滚动效果
+﻿// 问题:无法实现滚动效果
 window.onload = function () {
     new IScroll(document.querySelector(".zh_scroll"), {
         scrollX: true,
-        scrollY: false
+        scrollY: false,
+        bounce: false
     });
     new IScroll(document.querySelector(".zh_sortLeft"), {
         scrollX: false,
@@ -13,6 +14,21 @@ window.onload = function () {
         scrollY: true
     });
 }
-$(function(){
-    $('.carousel').carousel()
+$(function () {
+    var $carousels = $(".carousel");
+    var starX, endX;
+    var offset = 30;
+    // 注册滑动事件
+    $carousels.on("touchstart", function (e) {
+        starX = e.originalEvent.touches[0].clientX;
+    });
+    $carousels.on("touchmove", function (e) {
+        endX = e.originalEvent.touches[0].clientX;
+    });
+    $carousels.on("touchend", function (e) {
+        var distance = Math.abs(starX - endX);
+        if (distance > offset) {
+            $(this).carousel(starX > endX ? "next" : "prev");
+        }
+    });
 });
